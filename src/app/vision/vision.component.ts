1a1bcc1177ab5e56bb2d90134e5e5fc2e9bae405
Adam Tse <mr.adam.tse@gmail.com>
2017-03-18 21:09:12 -0400

    added google form

--- src/app/vision/vision.component.ts
+++ src/app/vision/vision.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vision.component.css']
 })
 export class VisionComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
